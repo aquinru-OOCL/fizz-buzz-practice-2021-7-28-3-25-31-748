466e59d01c609de842eb335accb0527cf45f5f4a
AQUINRU <russel.aquino@oocl.com>
2021-08-05 13:48:48 +0800

    [FizzBuzz][Russel] feat: multiple of 3 implementation

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,9 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
     public String countOff(int orderNumber) {
 
+        if (orderNumber % 3 == 0) {
+            return "Fizz";
+        }
 
         return String.valueOf(orderNumber);
     }
