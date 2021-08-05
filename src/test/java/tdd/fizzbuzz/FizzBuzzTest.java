6daed143219382733d674cc66f794f68386bc6d6
AQUINRU <russel.aquino@oocl.com>
2021-08-05 13:43:39 +0800

    [FizzBuzz][Russel] test: write normal case

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,23 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class FizzBuzzTest {
+    FizzBuzz fizzBuzz = new FizzBuzz();
+    int orderNumber;
+    String result;
+
+    @Test
+    void should_return_order_number_when_count_off_given_normal_case() {
+        //Given
+        orderNumber = 1;
+
+        //When
+        result = fizzBuzz.countOff(orderNumber);
+
+        //Then
+        assertThat(result).isEqualTo("1");
+    }
 }
